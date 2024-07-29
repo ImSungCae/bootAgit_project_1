f0054ed4f09ae39b1768949c9609ff9f06809a39
ImSungCae <lschmhj@naver.com>
2024-07-29 17:25:44 +0900

    SpringBoot + JWT + React
    WebSocket, STOMP 활용한 REACT 시작

---
+++ src/main/java/com/bootagit_project_1/chat/ChatDto.java
@@ -0,0 +1,11 @@
+package com.bootagit_project_1.chat;
+
+import lombok.Data;
+
+@Data
+public class ChatDto {
+    private Integer channelId;
+    private Integer writerId;
+    private String chat;
+
+}
